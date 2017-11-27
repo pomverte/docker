3416c4561aad9780f0c2eec265b437499ea49e6c
hvle <pomverte@users.noreply.github.com>
2017-11-27 10:31:40 +0100

    Create install-docker-centos.sh

---
+++ install-docker-centos.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+yum update -y
+yum install -y epel-release
+
+## DOCKER CE
+# https://docs.docker.com/engine/installation/linux/docker-ce/centos/
+yum remove docker \
+    docker-common \
+    docker-selinux \
+    docker-engine
+
+yum install -y yum-utils \
+    device-mapper-persistent-data \
+    lvm2
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y docker-ce
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+
+## DOCKER-COMPOSE
+# https://docs.docker.com/compose/install/
+curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
