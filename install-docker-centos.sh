93ab055cf520c4434dc1993328d0a2a866c9e29f
hvle <pomverte@users.noreply.github.com>
2018-08-07 11:53:28 +0200

    Update install-docker-centos.sh

--- install-docker-centos.sh
+++ install-docker-centos.sh
@@ -38,4 +38,3 @@ sudo systemctl start docker
 # https://docs.docker.com/compose/install/#install-compose
 curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-docker-compose --version
