e54539876025ada3daffed1123b19e9c867f3e5d
hvle <pomverte@users.noreply.github.com>
2019-03-18 14:34:38 +0100

    upgrade docker-compose version 1.24.0

--- install-docker-centos.sh
+++ install-docker-centos.sh
@@ -2,7 +2,7 @@
 
 # UPDATE : checkout https://get.docker.com  https://github.com/docker/docker-install
 
-DOCKER_COMPOSE_VERSION=1.21.2
+DOCKER_COMPOSE_VERSION=1.24.0
 
 yum update -y
 yum install -y epel-release
@@ -16,8 +16,6 @@ sudo yum remove docker \
     docker-latest \
     docker-latest-logrotate \
     docker-logrotate \
-    docker-selinux \
-    docker-engine-selinux \
     docker-engine
 
 sudo yum install -y yum-utils \
@@ -28,7 +26,7 @@ sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install -y docker-ce
+sudo yum install docker-ce docker-ce-cli containerd.io
 
 sudo systemctl enable docker
 sudo systemctl start docker
@@ -36,5 +34,10 @@ sudo systemctl start docker
 
 ## DOCKER-COMPOSE
 # https://docs.docker.com/compose/install/#install-compose
-curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# add user to docker group
+# https://docs.docker.com/install/linux/linux-postinstall/
+sudo usermod -aG docker $USER
+# logout and log in back again
