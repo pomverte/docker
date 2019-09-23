03f4aeb9dacb6a065f5b59ba5542495aadb730a9
hvle <pomverte@users.noreply.github.com>
2019-09-23 17:49:05 +0200

    yum install no prompt

--- install-docker-centos.sh
+++ install-docker-centos.sh
@@ -26,7 +26,7 @@ sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install -y docker-ce docker-ce-cli containerd.io
 
 sudo systemctl enable docker
 sudo systemctl start docker
